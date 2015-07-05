c511a2ec7c9452427e523777f809e2de2cb3540e
Jim Shank <jim@theshanks.net>
2015-07-05 13:24:00 -1000

    Delete test.pl
    No longer needed, found better ways.

--- test.pl
+++
@@ -1,9 +0,0 @@
-use XML::SAX;
-  use MySAXHandler;
-  
-  my $parser = XML::SAX::ParserFactory->parser(
-        Handler => MySAXHandler->new
-  );
-  
-  $parser->parse_uri("foo.xml")
-/ISAPI/Event/notification/alertStream
