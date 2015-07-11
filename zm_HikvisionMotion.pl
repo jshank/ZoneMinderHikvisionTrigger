0bd30dd4ff998b8c13dc4317ae9dec32c3bdf967
Jim Shank <jim@theshanks.net>
2015-07-10 21:05:04 -0700

    Clean-up and commenting of modules, preparing for Coro implementation

--- zm_HikvisionMotion.pl
+++ zm_HikvisionMotion.pl
@@ -22,6 +22,11 @@ Summary: You need to set the Hikvision as follow:
 Configuration -> Advanced Configuration:
 	Basic Event:
 		[X] Enable Motion Detection
+Configuration -> Security
+	User:
+		Level: Operator
+		Basic Permissions
+			[X] Remote: Notify Surveillance Center / Trigger Alarm Output
 Draw the area you would like to enable motion detection for then click Stop Drawing
 Set sensitivity to 60
 Make sure Arming Schedule is set to all hours, all times (unless you want motion detection only during certain hours)
@@ -30,25 +35,20 @@ Save
 
 use warnings;
 use strict;
-use Socket;
+use Socket; #For calling out to zm_trigger over tcp
 use ZoneMinder::Logger qw(:all);
-use XML::Twig;
-use LWP;
-use YAML::XS 'LoadFile';
+use XML::Twig; #Handles our XML from the camera
+use LWP; #Talks to the camera over HTTP
+use MultipartFilter; #Proper handling of multipart HTTP
+use Data::Dumper; #Debugging and diagnostice **Not needed for runtime**
+use Config::JSON; # For storing configuration in JSON
+# Next 3 packages are for supporting multiple cameras at once.
+use Coro;
+use Coro::LWP;
+use EV;
 
-my $config = LoadFile('config.yaml');
-# This is the URL that streams alerts such as motion detection. Documentation at http://goo.gl/S38ZQq
-my $url = $config->{alertStreamUrl};
-my $browser = LWP::UserAgent->new();
-my $ipAddress;
+my $config = Config::JSON->new('config.json');
 my $lastEvent = "videoloss";
-my $firstResponse = 1;
-my $zm_trigger_ip = $config->{zm_trigger_ip};
-my $zm_trigger_port = $config->{zm_trigger_port}; 
-my $sql_auth = "-u".$config->{sql_credentials}->{username}." -p".$config->{sql_credentials}->{password};
-my $secondsBeforeRecording = 3;
-my $eventStartTime;
-my @monitors = $config->{monitors};
 
 # Create a Twig handler that will parse out the details of the event 
 my $twig =
@@ -56,41 +56,38 @@ my $twig =
     twig_handlers => { EventNotificationAlert => \&AlertStreamHandler } );
 
 # Create an LWP instance that will open a connection to the camera and
-# read the alertStream in 1024 byte chunks and hand them to the raw_handler.
+# read the alertStream via MultipartFilter and hand off our XML to AlertStreamHandler
 # This will automatically seperate the stream into parts per 
 # http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.2
 #$browser->credentials($alertStreamHost.":".$alertStreamPort, '', $alertStreamUsername=>$alertStreamPassword);
-my $response = $browser->get(
-    #$alertStreamUrl,
-    $url,
-    ':content_cb'     => \&raw_handler,
-    ':read_size_hint' => 1024,
-);
-
-
-=item raw_handler()
-
-This handler takes the data read from the camera's alertStream and sends
-the XML part along to the AlertStreamHandler via twig->parse
-
-=cut
-
-sub raw_handler {
-    my ( $data, $response ) = @_;
-	if ($firstResponse) {
-		if ($response->is_success) {
-		     print "Connected to camera\n";
-			$firstResponse = 0;
-		 }
-		 else {
-		     die $response->status_line;
+foreach my $monitor ($config->get("monitors")) { 
+	foreach my $monitorvalues ($monitor) {
+		while(my($ipAddress, $details) = each $monitorvalues ) {
+			#Key is now the ipAddress of each monitor, value is a hash of the attributes.
+			StartMonitor($ipAddress, $details);
 		 }
 	}
-    # handle the payload and ignore the headers that start with --boundary
-    unless ( $data =~ /^--boundary/ ) {
-        $twig->parse($data);
-    }
-}
+};
+
+
+
+sub StartMonitor {
+	my ( $monitorIp, $monitorDetails) = @_;
+	my $alertStreamUrl = "http://".$monitorDetails->{ 'username' }.":".$monitorDetails->{ 'password' }."@".$monitorIp."/Event/notification/alertStream";
+	#print "Monitor IP: ".$monitorIp."\n";
+	print "Starting monitor: ".$alertStreamUrl."\n";
+	my $browser = LWP::UserAgent->new();
+	MultipartFilter->hookInto(
+		$browser,
+    		onDocument => sub {
+        		my $part=shift;
+        		$twig->parse($part->content());
+    		}
+	);
+
+	my $response = $browser->get($alertStreamUrl);
+	print Dumper($response);
+};
 
 =item AlertStreamHandler()
 Extracts the fields we're looking for from the AlertStream XML chunk
@@ -103,6 +100,7 @@ sub AlertStreamHandler {
     my ( $twig, $eventAlert ) = @_;
     my $ip        = $eventAlert->first_child('ipAddress')->text;
     my $eventType = $eventAlert->first_child('eventType')->text;
+    my $eventStartTime;
     unless ($eventType eq $lastEvent) {
         $lastEvent = $eventType;
 	my $timeStamp = $eventAlert->first_child('dateTime')->text;
@@ -123,7 +121,8 @@ sub AlertStreamHandler {
 }
 
 
-=for comment{
+=for comment
+Hanging on to this string for possible future use
 	$is_nodect=`/usr/bin/mysql -D zm $sql_auth -s -N -e  \"SELECT CONCAT(Enabled,Function) FROM Monitors WHERE Id=$iter->{id};\"`;
 	chomp($is_nodect);
 	if ($is_nodect ne "1Nodect")
@@ -144,6 +143,8 @@ sub AlertStreamHandler {
 
 sub zm_trigger {
 	my ( $monitorId, $recordState ) = @_;
+	my $zm_trigger_ip = $config->get("zm_trigger_ip");
+	my $zm_trigger_port = $config->get("zm_trigger_port"); 
 	my $sock = IO::Socket::INET->new(PeerAddr => $zm_trigger_ip,
 				 	PeerPort => $zm_trigger_port,
 					Proto    => 'tcp');
@@ -156,45 +157,9 @@ sub zm_trigger {
 		print "Success connecting to ZM_TRIGGER\n";
 		my $string_to_write = $monitorId."|".$recordState."|1|External Motion|External Motion";
 		print "Sending $string_to_write\n";
-		$sock->send($string_to_write);
-		$sock->close();
-		Info ("ARC:Sending string: $string_to_write. I'll wait for a while before I poll again");
+		#$sock->send($string_to_write);
+		#$sock->close();
+		#Info ("ARC:Sending string: $string_to_write. I'll wait for a while before I poll again");
 	}
 
 }
-=for comment
-  
-
-	# 0 = motion not enabled, 1 = enabled but not detected, 2 = enabled and detected
-	my ($motionvalue) = $contents =~ /<motionDetectAlarm>(.*)<\/motionDetectAlarm>/;
-
-	if ($motionvalue eq "2")
-	{
-		#print "Motion detected for ".$iter->{name}."\n";
-		Info( "ARC:Motion detected for $iter->{name}");
-		if ($iter->{state} eq "off")
-		{
-	}
-
-		else
-		{
-			Debug($iter->{name}." Already recording, not asking again\n");
-		}
-	}
-	elsif ($motionvalue eq "1")
-	{
-		#print "Motion NOT detected for ".$iter->{name}."\n";
-		if ($iter->{state} eq "on")
-		{
-	}
-		else
-		{
-			#print $iter->{name}." is already stopped\n";
-		}
-	}
-	else
-	{
-		Debug("Motion is NOT ENABLED for ".$iter->{name});
-	}
-
-=cut
